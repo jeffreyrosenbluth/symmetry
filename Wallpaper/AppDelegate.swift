eaa3f87e1128ac4cf28bae28b2856f8d1c51b19e
Jeffrey Rosenbluth <jeffrey.rosenbluth@gmail.com>
2018-05-25 09:26:12 -0400

    Initial Commit

---
+++ Wallpaper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Wallpaper
+//
+//  Created by Jeffrey Rosenbluth on 5/25/18.
+//  Copyright © 2018 Applause Code. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Wallpaper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Wallpaper
+//
+//  Created by Jeffrey Rosenbluth on 5/25/18.
+//  Copyright © 2018 Applause Code. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
