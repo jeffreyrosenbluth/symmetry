244d1c23e56eaa8c0cefeff5aedc461b6e63189b
Jeffrey Rosenbluth <jeffrey.rosenbluth@gmail.com>
2018-07-03 10:10:55 -0600

    group working

--- Wallpaper/DetailViewController.swift
+++ Wallpaper/DetailViewController.swift
@@ -11,19 +11,6 @@ import Cocoa
 class DetailViewController: NSViewController {
     @IBOutlet var imageView: NSImageView!
     
-    @IBAction func loadImage(_ sender: NSMenuItem) {
-        guard let url = NSOpenPanel().selectUrl else { return }
-        guard let image = NSImage(contentsOf: url) else { return }
-        
-        let c0 = Coef(nCoord: 1, mCoord: 0, anm: Complex(0.75, 0.25))
-        let c1 = Coef(nCoord: -2, mCoord: 2, anm: Complex(0.2, -0.2))
-        let c2 = Coef(nCoord: 1, mCoord: -1, anm: Complex(0.6, 0.1))
-        let opts = Options(width: 600, height: 480, repLength: 100, scale: 0.5, rotation: Double.pi/6)
-        
-        let paper = wallpaper(options: opts, recipeFn: p4m, coefs: [c0, c1, c2], preProcess: antiSymmVertical, nsImage: image)
-        imageView.image = paper
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
